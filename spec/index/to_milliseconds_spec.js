599ea10d7ab6a2b1764e770efee7c64d74b08ca8
Tony Fabeen <tony.fabeen@gmail.com>
2015-04-01 14:26:02 -0300

    throw new Error

--- spec/index/to_milliseconds_spec.js
+++ spec/index/to_milliseconds_spec.js
@@ -1,10 +1,18 @@
 var benx = require('../../index');
 
 describe('toMilliseconds', function() {
-  it('returns data in milliseconds', function() {
-    var seconds = 7;
-    var nanoseconds = 350164042;
+  describe('when it does not receive arguments', function() {
+    it('throws exception', function() {
+      expect(benx.toMilliseconds).toThrow(new Error('Arguments should be passed'));
+    });
+  });
 
-    expect(benx.toMilliseconds(seconds, nanoseconds)).toEqual(7350);
+  describe('on success', function() {
+    it('returns data in milliseconds', function() {
+      var seconds = 7;
+      var nanoseconds = 350164042;
+
+      expect(benx.toMilliseconds(seconds, nanoseconds)).toEqual(7350);
+    });
   });
 });
